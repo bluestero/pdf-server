010cc86a386ebfb074b10d80fb9f92c06289780c
Ahmed Khatib <48290984+bluestero@users.noreply.github.com>
2024-05-02 15:35:57 +0530

    Updated the setup.sh config.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 nix-env -iA nixpkgs.jdk17
+nix-env -iA nixpkgs.gnumake
+nix-env -iA nixpkgs.libgcc
+nix-env -iA nixpkgs.automake
+nix-env -iA nixpkgs.autoconf
 nix-env -iA nixpkgs.libtool
+nix-env -iA nixpkgs.pkg-config
+nix-env -iA nixpkgs.zlib
 nix-env -iA nixpkgs.leptonica
 nix-env -iA nixpkgs.libreoffice
-nix-env -iA nixpkgs.unoconv
-nix-env -iA nixpkgs.pngquant
 nix-env -iA nixpkgs.unpaper
 nix-env -iA nixpkgs.ocrmypdf
-pip3 install opencv-python-headless
+nix-env -iA nixpkgs.python312
+pip3 install uno opencv-python-headless unoconv pngquant WeasyPrint --break-system-packages
 
 mkdir .git
 cd .git
